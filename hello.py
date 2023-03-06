095988e34b42dcad2765b9daf4cdd4987744eafe
Mary Wachiuri <wachiurimary@gmail.com>
2023-03-06 11:24:52 +0300

    flask app

--- hello.py
+++ hello.py
@@ -1,7 +1,54 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
+# flash -> flash messages
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
 
 # Create a Flask instance
 app = Flask(__name__, template_folder='templates')
+app.config['SECRET_KEY'] = "super secret key that should be hidden" # secret key for csrf token
+
+# Create a Form Class
+class NamerForm(FlaskForm):
+    name = StringField("What's your name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+    
+    # Fields
+        # BooleanField
+        # DateField
+        # DateTimeField
+        # DecimalField
+        # FileField
+        # HiddenField
+        # MultipleField
+        # FieldList
+        # FloatField
+        # FormField
+        # IntegerField
+        # PasswordField
+        # RadioField
+        # SelectField
+        # SelectMultipleField
+        # SubmitField
+        # StringField
+        # TextAreaField
+    
+    # Validators
+        # DataRequired
+        # Email
+        # EqualTo
+        # InputRequired
+        # IPAddress
+        # Length
+        # MacAddress
+        # NumberRange
+        # Optional
+        # Regexp
+        # URL
+        # UUID
+        # AnyOf
+        # NoneOf
 
 # Create a route decorator
 @app.route('/')
@@ -51,7 +98,22 @@ def page_not_found(e):
     return render_template("404.html"), 404
 
 
-# 1. Internal Server Error
+# 2. Internal Server Error
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
+
+
+# Create Name Page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    # validate the form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        flash("Form Submitted Successfully!") # our flash message
+    return render_template("name.html",
+                           name = name,
+                           form = form)
